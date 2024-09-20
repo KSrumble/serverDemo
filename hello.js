470e36753cc958e818ae9feaeb0bdc3c5961ae42
Yeo Kai Shun <yeokaishun96@gmail.com>
2024-09-21 07:38:25 +0800

    2nd commit

--- hello.js
+++ hello.js
@@ -1,6 +1,10 @@
 //const express = require('express');
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
+
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
